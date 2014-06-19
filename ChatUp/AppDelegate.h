ed98f4606f9c3fa8d91056a49fa523fb77aae471
Rakshit Pithadia <rakshitpithadia@Rakshits-MacBook-Pro.local>
2014-06-19 16:52:16 -0700

    Initial Commit

---
+++ ChatUp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChatUp
+//
+//  Created by Rakshit Pithadia on 6/19/14.
+//  Copyright (c) 2014 Rakshit Pithadia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChatUp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ChatUp
+//
+//  Created by Rakshit Pithadia on 6/19/14.
+//  Copyright (c) 2014 Rakshit Pithadia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
